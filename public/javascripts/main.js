cabed89d777b170048c10d73c3743bff9fff7882
Alistair McLean <al.mclean@gmail.com>
2014-06-21 11:20:40 -0400

    Konig-27 Create a heartbeat in the client to the DB - Added heartbeat of 2 seconds.  Tested locally.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,33 @@
 
 // Event Handlers
 $(function () {
-    "use strict";
-    console.log('Page is now ready !');
+    var neoBaseUrl = "http://localhost:7474/db/data/";
+
+    // Ping the base url for connectivity.
+    setInterval(function() {
+        var uptimePingRequest = $.get(neoBaseUrl);
+
+        uptimePingRequest.success(function() {
+            if ($('#srvStatus').hasClass('label-danger')) {
+                console.log('DB Connection Ok');
+                $('#srvStatus')
+                    .removeClass('label-danger')
+                    .addClass('label-success')
+                    .text('Connection Ok');
+            }
+        });
+
+        uptimePingRequest.error(function() {
+            "use strict";
+            if ($('#srvStatus').hasClass('label-success')) {
+                console.log('DB Connection Failure');
+                $('#srvStatus')
+                    .removeClass('label-success')
+                    .addClass('label-danger')
+                    .text('No Connection');
+            }
+        });
+
+    }, 2000);
+
 });
