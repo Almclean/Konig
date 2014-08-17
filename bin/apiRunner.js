95699627f0e7f23498da965815b1b5c8fe3e6edf
Alistair McLean <al.mclean@gmail.com>
2014-08-17 13:56:22 -0400

    Can now query properly and get back sensible object.

--- bin/apiRunner.js
+++ bin/apiRunner.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 // Tool for running the api commands on the command line !
-var _serviceRoot = 'http://localhost:7474/db/data/';
-var api = new (require('../services/api'))(_serviceRoot);
+var qs = new (require('../services/QueryService'))();
 
 function main() {
     "use strict";
-
-    api.getMetaData().then(function (result) {
-        console.log(JSON.stringify(result, null, '\t'))
-    });
+    qs.loadByTitle("Party by Location");
 }
 
 main();
--- services/QueryService.js
+++ services/QueryService.js
@@ -13,6 +13,26 @@ function QueryService () {}
 
 QueryService.prototype.loadByTitle = function (title) {
     var queryText = [
-        'MATCH '
-    ]
+        'MATCH (q:Query {title: {title} })-[:COMPRISED_OF]->(t:Triplet)',
+        'RETURN q, collect(t) as triplets'
+    ].join('\n');
+    return apiInstance.query(queryText, {title: title})
+        .then(function (results) {
+            var queries = [];
+            for (var query in results) {
+                if (results[query].hasOwnProperty("q")) {
+                    var newQuery = results[query].q._data.data;
+                    var triplets = {};
+                    for (var i = 0; i < results[query].triplets.length; i++) {
+                        triplets["triplet" + i] = results[query].triplets[i]._data.data;
+                    }
+                    newQuery.triplets = triplets;
+                    queries.push(newQuery);
+                }
+            }
+            console.log(JSON.stringify(queries, null, "\t"));
+        }
+    );
 };
+
+module.exports = QueryService;
