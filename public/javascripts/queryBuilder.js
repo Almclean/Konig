6f2faf38c71de8ed0edfa54f7af5ad9f60aa1a16
omahivan <moi.b9.34@gmail.com>
2014-09-02 22:26:15 -0400

    Added basic graph rendering. Need to parse out data next

--- public/javascripts/queryBuilder.js
+++ public/javascripts/queryBuilder.js
@@ -37,9 +37,15 @@ $(function () {
             if (data && data.length > 0) {
                 for (var i = 0; i < data.length; i++) {
                     console.log(JSON.stringify(data[i]));
+                    console.log("Result");
                 }
             }
         });
+        $('#graph').show()
+            .css('background-color', 'white')
+            .css('width', '50%')
+            .css('height', '50%');
+        drawGraph(graph);
     });
 
     $('#querySave').on('click', function (event) {
@@ -118,4 +124,85 @@ $(function () {
         }
         return mm + '-' + dd + '-' + now.getFullYear() + '-' + now.getTime();
     }
+
+    var graph = {
+        "nodes": [
+            {"name": "Myriel", "group": 1},
+            {"name": "Napoleon", "group": 2},
+            {"name": "Mlle.Baptistine", "group": 3}
+        ],
+        "links": [
+            {"source": 0, "target": 1, "value": 1},
+            {"source": 1, "target": 2, "value": 8},
+            {"source": 2, "target": 0, "value": 10}
+        ]
+    };
+
+    // Graph stuff
+    var width = 960,
+        height = 500;
+
+    var color = d3.scale.category20();
+
+    var force = d3.layout.force()
+        .charge(-100)
+        .linkDistance(150)
+        .size([width, height]);
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var drawGraph = function (graph) {
+        force
+            .nodes(graph.nodes)
+            .links(graph.links)
+            .start();
+
+        var link = svg.selectAll(".link")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function (d) {
+                return Math.sqrt(d.value);
+            });
+
+        var gnodes = svg.selectAll('g.gnode')
+            .data(graph.nodes)
+            .enter()
+            .append('g')
+            .classed('gnode', true);
+
+        var node = gnodes.append("circle")
+            .attr("class", "node")
+            .attr("r", 10)
+            .style("fill", function (d) {
+                return color(d.group);
+            })
+            .call(force.drag);
+
+        gnodes.append("text")
+            .text(function (d) {
+                return d.name;
+            });
+
+        force.on("tick", function () {
+            link.attr("x1", function (d) {
+                return d.source.x;
+            })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            gnodes.attr("transform", function (d) {
+                return 'translate(' + [d.x, d.y] + ')';
+            });
+        });
+    };
 });
