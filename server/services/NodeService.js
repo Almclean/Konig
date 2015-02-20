4fda15a4114a02895db3a23f90f6ab8c0991fb93
Alistair McLean <al.mclean@gmail.com>
2015-02-20 18:47:46 -0500

    Konig-52 Create Node API : Started creation of NodeService.

---
+++ server/services/NodeService.js
@@ -0,0 +1,21 @@
+/**
+ * Created by almclean on 20/02/15.
+ */
+/*jslint node: true */
+"use strict";
+
+function NodeService() { }
+
+NodeService.prototype.createNode = function () {
+    // Not yet implemented
+    // From github
+    // POST /entity/Party : data : {blah : "blah"}
+    // POST /relationship/123/OWNS/124
+    return null;
+};
+
+NodeService.prototype.updateNode = function () {
+    // Not yet implemented
+    return null;
+};
+
