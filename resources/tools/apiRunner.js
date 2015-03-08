ba78e10edc5f37352420dc8d11b52d8365388ee9
Alistair McLean <al.mclean@gmail.com>
2015-03-08 16:15:26 -0400

    Konig-52 Create Node API : Added Node create that returns the Node Metadata : {ID & Labels}

--- resources/tools/apiRunner.js
+++ resources/tools/apiRunner.js
@@ -1,25 +1,32 @@
 #!/usr/bin/env node
 // Tool for running the api commands on the command line !
-var Promise = require('bluebird');
-var r = Promise.promisifyAll(require('request'));
-var bcrypt = require('bcrypt');
 var api = new (require('../../server/services/Api'))();
+var util = require('util');
 var _ = require('lodash');
 
 function main() {
-  "use strict";
+    "use strict";
     console.log('About to submit query');
+    var label = "TEST";
+    var data = {name: "Al", text: "This is some random text"};
 
-    bcrypt.hash("blah", 10, function(err, hash) {
-        console.log(hash);
-    });
+    var queryText = [
+        util.format("CREATE (n:%s {data})", label),
+        "RETURN n"
+    ].join("\n");
 
-    console.log("Password matches hash : " + bcrypt.compareSync("blah", "$2a$10$mlXKznVWibJ7bQDLMwP/6eV4RZvWxVLw1h4UNKnDbmOfYVQOHfwKq"));
-
-  // Send a post to the routing service
-  // Find the query
-  // bind the triplet information to the query ?
-  // Return using getNodes.
+    console.time("queryTime");
+    api.query(queryText, {data: data})
+        .then(function (results) {
+            console.log("Got something back.");
+            if (results && results.data) {
+                console.log(_.first(_.flatten(results.data)).metadata);
+                console.timeEnd("queryTime");
+            } else if (results.exception) {
+                console.error(results.message);
+                throw results.exception;
+            }
+        });
 
 }
 
--- server/services/NodeService.js
+++ server/services/NodeService.js
@@ -4,18 +4,41 @@
 /*jslint node: true */
 "use strict";
 
+var util = require("util");
+var Api = require("./Api");
+var _ = require("lodash");
+var apiInstance = new Api();
+
 function NodeService() { }
 
-NodeService.prototype.createNode = function () {
-    // Not yet implemented
-    // From github
-    // POST /entity/Party : data : {blah : "blah"}
-    // POST /relationship/123/OWNS/124
-    return null;
+
+// @Param label : Type of node to create
+// @Param data : data to populate node with
+NodeService.prototype.createNode = function (label, data) {
+    var queryText = [
+        util.format("CREATE (n:%s {data})", label),
+        "RETURN n"
+    ].join("\n");
+
+    return apiInstance.query(queryText, {data: data})
+        .then(function (results) {
+            if (results && results.data) {
+                return _.first(_.flatten(results.data)).metadata;
+            } else if (results.exception) {
+                throw results.exception;
+            }
+        });
 };
 
-NodeService.prototype.updateNode = function () {
-    // Not yet implemented
+NodeService.prototype.updateNode = function (nodeid, data) {
+
+    var queryText = [
+        "",
+        ""
+    ].join("\n");
+
+    return apiInstance.query(queryText)
+
     return null;
 };
 
