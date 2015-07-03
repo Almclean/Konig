0742057a35190ae60b555ed036be21968d48aa4c
omahivan <moi.b9.34@gmail.com>
2015-07-03 15:32:56 -0400

    Removing old NodeJS GraphTransformer and replacing it with new Python one

--- server/services/GraphTransformer.py
+++ server/services/GraphTransformer.py
@@ -2,15 +2,19 @@ __author__ = 'Ivan'
 
 import logging
 from server.error import GraphTransformerException
+import json
+from pydash.objects import deep_get
 
 log = logging.getLogger(__name__)
 
-class GraphTransformer(object):
 
+class GraphTransformer(object):
     @staticmethod
     def to_client_graph(data):
         """
-        Takes a given a server graph, in JSON format and transforms it into a client side graph, in JSON format
+        Takes a given a server graph, in JSON format and transforms it into a client side graph, in JSON format.
+        NOTE: The client side graph here is in a vaild D3 format of nodes and links, more details in
+        public/javascripts/QueryEditor.js
         :param data: The server side, e.g. data container representation of the data
         :return: JSON data in the client format
         (http://docs.python-requests.org/en/latest/user/quickstart/#errors-and-exceptions)
@@ -18,5 +22,33 @@ class GraphTransformer(object):
         if not data:
             log.error("No data to transform")
             raise GraphTransformerException("No data to transform")
+        nodes = {}
+        triplets = deep_get(data, 'triplets')
+        for trip in triplets:
+            nodes[deep_get(trip, '0.source.url')] = deep_get(trip, '0.source')
+            nodes[deep_get(trip, '2.target.url')] = deep_get(trip, '2.target')
 
-        return data
+        uniq_nodes = []
+        for node in nodes.values():
+            uniq_nodes.append({"name": node['data']['name'], "group": node['label'], "url": node['url']})
+
+        links = []
+        for trip in triplets:
+            links.append({"source": GraphTransformer.index_of_node(uniq_nodes, deep_get(trip, '0.source.url')),
+                          "target": GraphTransformer.index_of_node(uniq_nodes, deep_get(trip, '2.target.url')),
+                          "value": 1,
+                          "url": deep_get(trip, '1.relationship.url')})
+
+        return {"nodes": uniq_nodes, "links": links}
+
+    @staticmethod
+    def index_of_node(nodes, url):
+        """
+       Given a list of dictionaries and a url to find in a dictionary returns the index of the matching dictionary
+       :param nodes: The list of nodes
+       :param nodes: The node_key to find the index of
+       :return: Int the index of the node_key in the list
+       """
+        for i, k in enumerate(nodes):
+            if k['url'] == url:
+                return i
--- tests/unit/GraphTransformer_tests.py
+++ tests/unit/GraphTransformer_tests.py
@@ -3,8 +3,43 @@ __author__ = 'Ivan'
 from server.services.GraphTransformer import GraphTransformer
 from server.error import GraphTransformerException
 from nose.tools import raises
+from sure import expect
+import json
+import os
+
 
 @raises(GraphTransformerException)
 def test_no_server_data():
     """Tests that the GraphTransformer throws and exception when no data is passed"""
     GraphTransformer.to_client_graph(None)
+
+
+def test_single_triplet():
+    """Tests that the GraphTransformer can parse a single triplet """
+    dir = os.path.dirname(__file__)
+    with open(os.path.join(dir, 'json' + os.sep + 'gt_valid_single.json')) as data_file:
+        data = json.load(data_file)
+        actual = GraphTransformer.to_client_graph(data)
+        with open(os.path.join(dir, 'json' + os.sep + 'gt_valid_single_trans.json')) as data_file:
+            expected = json.load(data_file)
+            expect(actual).to.equal(expected)
+
+def test_multiple_triplet():
+    """Tests that the GraphTransformer can parse a single triplet """
+    dir = os.path.dirname(__file__)
+    with open(os.path.join(dir, 'json' + os.sep + 'gt_valid_multiple.json')) as data_file:
+        data = json.load(data_file)
+        actual = GraphTransformer.to_client_graph(data)
+        expect(actual['links'].__len__()).to.equal(3)
+        expect(actual['nodes'].__len__()).to.equal(5)
+        # TODO Do we really need to validate content here?
+        with open(os.path.join(dir, 'json' + os.sep + 'gt_valid_multiple_trans.json')) as data_file:
+            expected = json.load(data_file)
+            expect(actual).to.equal(expected)
+
+def test_index_of_node():
+    """Tests that the GraphTransformer can parse a single triplet """
+    nodes = [{'url': 'al'}, {'url': 'tony'}, {'url': 'ivan'}, {'url': 'dan'}]
+    key = "ivan"
+    index = GraphTransformer.index_of_node(nodes, key)
+    expect(index).to.equal(2)
